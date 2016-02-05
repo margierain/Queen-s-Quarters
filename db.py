55be614e4eca07aea730086f6f71fde739cc57a3
margierain <margaretrain.mo@gmail.com>
2016-02-05 07:48:42 +0300

    changes to key files

---
+++ db.py
@@ -0,0 +1,16 @@
+# from app import db
+# from models import role
+
+# # create database and tables
+# db.create_all()
+
+
+
+# # insert  data into rows
+# db.session.add([])
+
+
+
+
+# # commit changes
+# db.session.commit()
